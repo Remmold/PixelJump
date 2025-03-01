56a94cf3a4b73701a15a478e5b34b7bc887d0b33
Andreas Johansson <Andreas_91_johansson@hotmail.com>
2025-03-01 15:50:07 +0100

    Added folder for playerscripts and Started refactoring the player movement script! if this project is gonna go anywhere i need to get more structure :)

--- Assets/Scripts/Player scripts/PlayerMovement.cs
+++ Assets/Scripts/Player scripts/PlayerMovement.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using System.Xml.Schema;
+using Unity.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -7,37 +9,65 @@ using UnityEngine.UI;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField]  float runSpeed = 5f;
-    [SerializeField] float jumpSpeed = 5f;
-    [SerializeField] Vector2 deathKick = new Vector2 (10f,10f);
+    #region Player Movement Speeds&benchmarks
+    //Actual variables affecting game
+    [SerializeField]  float Xspeed ; // The Xspeed
+    [SerializeField] float Yspeed; // The Yspeed
+    float climbSpeed = 2f; //Yspeed while climbing
+    
+    
+    //Benchmarks------------------------------------
+
+    const float XSPEEDBASE = 10f; // benchmark for regular Xspeed
+    const float YSPEEDBASE  = 10f; // benchmark for regular Yspeed
+    const float CLIMBSPEEDBASE = 2f; // benchmarks for Yspeed While Climbing
+    const float GRAVITYBASE = 3; // benchmark for regular gravity for the player
+    const float SPEEDMULTIPLIERBASE = 1f; //Benchmark for regular speed multiplier
+    float speedMultiplier; //Multiplier applied to player movementspeed  this is the  thing to alter for temporary boosts to movementspeed
+    
+    #endregion
+    #region  Special Effects Vectors
+    [SerializeField] Vector2 deathKick = new Vector2 (3f,8f); // Happens when player dies
+    #endregion
+
+    #region  Gun Related Variables
+
+    //These are leftover remnants of the tutorial game doesnt fit actual project and will be removes but kept for disecting the systems for learning
     [SerializeField] GameObject bullet;
     [SerializeField] Transform gun;
-    [SerializeField]GameSession session;
-    float climbSpeed = 2f;
-    Collider2D myCollider ;
-    float startingGravity;
+    #endregion
+    #region Colliders Body and Animation
+    Collider2D myColliderCapsule ;
+    float myGravity ;
     Vector2 moveInput;
     Rigidbody2D myRigidBody;
     Animator animator;
-    Collider2D myFeetCollider;
+    Collider2D myColliderFeet;
+    #endregion
 
-    float speedMultiplier = 0.6f;
-    //float regularSpeedMultiplier = 0.5f;
     bool isAlive = true;
     void Start()
     {
+       
         myRigidBody = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
-        myCollider = GetComponent<Collider2D>();
-        myFeetCollider = GetComponent<BoxCollider2D>();
+        myColliderCapsule = GetComponent<Collider2D>();
+        myColliderFeet = GetComponent<BoxCollider2D>();
+        SetMovementSpeed(); //Initialize base movmentspeeds to player object
+
+
+
         
-
-
-
-        startingGravity = myRigidBody.gravityScale;
-        speedMultiplier *= FindAnyObjectByType<GameSession>().speedMultiplier;
     }
-
+    //Sets the diffrent movement parameters
+    public void SetMovementSpeed(float x =XSPEEDBASE,float y = YSPEEDBASE,float climb = CLIMBSPEEDBASE,float multiplier = SPEEDMULTIPLIERBASE,float gravity = GRAVITYBASE)
+    {
+        Xspeed = x;   
+        Yspeed = y;
+        speedMultiplier = multiplier;
+        climbSpeed = climb;
+        myRigidBody.gravityScale = gravity;
+    }
 
     void Update()
     {
@@ -67,9 +97,9 @@ public class PlayerMovement : MonoBehaviour
     }
     void ClimbLadder()
     {
-        if(!myCollider.IsTouchingLayers(LayerMask.GetMask("Climb")))
+        if(!myColliderCapsule.IsTouchingLayers(LayerMask.GetMask("Climb")))
         {
-            myRigidBody.gravityScale = startingGravity;
+            myRigidBody.gravityScale = myGravity;
             animator.SetBool("isClimbing",false);
             return;
         }
@@ -82,14 +112,14 @@ public class PlayerMovement : MonoBehaviour
     }
     void OnJump(InputValue value)
     {
-        if(value.isPressed && myFeetCollider.IsTouchingLayers(LayerMask.GetMask("Ground","Climb")))
+        if(value.isPressed && myColliderFeet.IsTouchingLayers(LayerMask.GetMask("Ground","Climb")))
         {
-            myRigidBody.linearVelocity += new Vector2(0f,jumpSpeed);
+            myRigidBody.linearVelocity += new Vector2(0f,Yspeed);
         }
     }
     void Run()
     {
-        Vector2 playerVelocity = new Vector2 (moveInput.x * (runSpeed*speedMultiplier),myRigidBody.linearVelocity.y);
+        Vector2 playerVelocity = new Vector2 (moveInput.x * (Xspeed*speedMultiplier),myRigidBody.linearVelocity.y);
         myRigidBody.linearVelocity = playerVelocity;
 
         bool playerHasHorizontalSpeed = Mathf.Abs(myRigidBody.linearVelocity.x)> Mathf.Epsilon;
