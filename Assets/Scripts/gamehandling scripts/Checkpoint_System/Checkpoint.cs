1bded944128cba1c6bc19785e2ba395c5244461a
Andreas Johansson <Andreas_91_johansson@hotmail.com>
2025-03-05 21:32:15 +0100

    Added an animation to the checkpoint for when they are activated. they now have a pot cooking on top of them when active

--- Assets/Scripts/gamehandling scripts/Checkpoint_System/Checkpoint.cs
+++ Assets/Scripts/gamehandling scripts/Checkpoint_System/Checkpoint.cs
@@ -3,12 +3,18 @@ using UnityEngine;
 public class Checkpoint : MonoBehaviour
 {
     private bool isActive = true;
-    
+    Animator animator;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player") && isActive)
         {
+            animator.SetBool("isActivated",true);
             Debug.Log($"Checkpoint reached at {transform.position}");
+
             FindAnyObjectByType<Checkpoint_Handler>().UpdateCheckpoints(transform);
             isActive = false; // Prevent retriggering
         }
