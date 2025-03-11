38ab94d7bc8c267aeecaec076f8ab9b5ca91923d
Andreas Johansson <Andreas_91_johansson@hotmail.com>
2025-03-11 21:44:50 +0100

    Fixed chicken script to be modular rathen then silly and repetative.

--- Assets/Objects/Characters/Chicken/ChickenBehaviour.cs
+++ Assets/Objects/Characters/Chicken/ChickenBehaviour.cs
@@ -5,39 +5,50 @@ public class ChickenBehaviour : MonoBehaviour
 {
     Rigidbody2D myRigidBody;
     Animator animator;
+    float moveSpeed = 2;
+
     void Start()
     {
         myRigidBody = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
         StartCoroutine(MoveAndPeck());
     }
+
+    IEnumerator Move(int duration)
+    {
+        Vector3 vector = myRigidBody.transform.localScale;
+        animator.SetBool("isRunning", true);
+        myRigidBody.linearVelocity = new Vector2(moveSpeed * vector.x, 0); // Changed linearVelocity to velocity
+        
+        yield return new WaitForSeconds(duration);
+
+        animator.SetBool("isRunning", false);
+        myRigidBody.linearVelocity = Vector2.zero; // Stop movement properly
+    }
+
+    void Flip()
+    {
+        float horizontalSpeed = myRigidBody.linearVelocity.x; // Fixed incorrect variable
+        myRigidBody.transform.localScale = new Vector3(-myRigidBody.transform.localScale.x, 1, 1); // Flip properly
+    }
+
+    IEnumerator Peck(float duration)
+    {
+        animator.SetBool("isPecking", true);
+        yield return new WaitForSeconds(duration);
+        animator.SetBool("isPecking", false);
+    }   
+
     IEnumerator MoveAndPeck()
     {
-        animator.SetBool("isPecking",false);
-        animator.SetBool("isRunning",true);
-        myRigidBody.linearVelocity = new Vector2(2,0);
-        myRigidBody.transform.localScale = new Vector3(1,1,0);
-        yield
-        return new WaitForSeconds(2);
-        myRigidBody.linearVelocity = new Vector2(0,0);
-        animator.SetBool("isRunning",false);
-        animator.SetBool("isPecking",true);
-        yield
-        return new WaitForSeconds(2);
-        animator.SetBool("isPecking",false);
-        animator.SetBool("isRunning",true);
-        myRigidBody.linearVelocity = new Vector2(-2,0);
-        myRigidBody.transform.localScale = new Vector3(-1,1,0);
-        yield
-        return new WaitForSeconds(2);
-        myRigidBody.linearVelocity = new Vector2(0,0);
-        animator.SetBool("isRunning",false);
-        animator.SetBool("isPecking",true);
-        yield
-        return new WaitForSeconds(2);
-        StartCoroutine(MoveAndPeck());
+        while (true) // Keeps the chicken moving forever
+        {
+            yield return StartCoroutine(Move(1)); // ✅ Move first
+            yield return StartCoroutine(Peck(1)); // ✅ Then peck
+            Flip(); // ✅ Flip direction AFTER both actions complete
+        }
     }
-    // Update is called once per frame
+
     void Update()
     {
         
